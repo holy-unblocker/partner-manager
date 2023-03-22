8f6ceba52c9a0c2e8458032cb0496b8a4f83ebc6
David Reed <reedswork@proton.me>
2023-03-22 19:47:46 -0400

    separate domain commands

--- packages/server/src/allCommands.ts
+++ packages/server/src/allCommands.ts
@@ -1,2 +1,3 @@
 import "./commands/ping.js";
 import "./commands/organization.js";
+import "./commands/domain.js";
---
+++ packages/server/src/commands/domain.ts
@@ -0,0 +1,139 @@
+import {
+  commandIsMember,
+  commandIsOwner,
+  getCommandID,
+} from "../commandUtil.js";
+import { CommandSubOnly, registerCommand } from "../commands.js";
+import db from "../db.js";
+import {
+  SlashCommandBuilder,
+  SlashCommandStringOption,
+  SlashCommandSubcommandBuilder,
+} from "discord.js";
+
+registerCommand(
+  new CommandSubOnly(
+    new SlashCommandBuilder()
+      .setName("domain")
+      .setDescription("Domain management"),
+    {
+      data: new SlashCommandSubcommandBuilder()
+        .setName("add")
+        .addStringOption(
+          new SlashCommandStringOption()
+            .setName("id")
+            .setDescription("Short identifier for the organization")
+            .setRequired(true)
+        )
+        .addStringOption(
+          new SlashCommandStringOption()
+            .setName("domains")
+            .setDescription("Comma separated list of domains")
+            .setRequired(true)
+        )
+        .setDescription("Adds domains to your organization"),
+      execute: async (interaction) => {
+        const id = await getCommandID(interaction);
+        if (!id) return;
+
+        const domains = interaction.options
+          .getString("domains", true)
+          .replace(/\s/, "")
+          .split(", ");
+
+        if (!(await commandIsMember(interaction, id))) return;
+
+        for (const domain of domains)
+          await db.query(
+            "INSERT INTO DOMAINS (DOMAIN, ORGANIZATION) VALUES ($1, $2);",
+            [domain, id]
+          );
+
+        await interaction.reply({
+          content: `Added ${domains.length} domains.`,
+          ephemeral: true,
+        });
+      },
+    },
+    {
+      data: new SlashCommandSubcommandBuilder()
+        .setName("delete")
+        .addStringOption(
+          new SlashCommandStringOption()
+            .setName("id")
+            .setDescription("Short identifier for the organization")
+            .setRequired(true)
+        )
+        .addStringOption(
+          new SlashCommandStringOption()
+            .setName("domains")
+            .setDescription("Comma separated list of domains")
+            .setRequired(true)
+        )
+        .setDescription("Deletes domains from your organization"),
+      execute: async (interaction) => {
+        const id = await getCommandID(interaction);
+        if (!id) return;
+
+        const domains = interaction.options
+          .getString("domains", true)
+          .replace(/\s/, "")
+          .split(", ");
+
+        if (!(await commandIsMember(interaction, id))) return;
+
+        for (const domain of domains)
+          await db.query(
+            "DELETE FROM DOMAINS WHERE ORGANIZATION = $1 AND DOMAIN = $2;",
+            [id, domain]
+          );
+
+        await interaction.reply({
+          content: `Deleted ${domains.length} domains.`,
+          ephemeral: true,
+        });
+      },
+    },
+    {
+      data: new SlashCommandSubcommandBuilder()
+        .setName("clear")
+        .addStringOption(
+          new SlashCommandStringOption()
+            .setName("id")
+            .setDescription("Short identifier for the organization")
+            .setRequired(true)
+        )
+        .setDescription("Deletes all the domains your organization"),
+      execute: async (interaction) => {
+        const id = await getCommandID(interaction);
+        if (!id) return;
+
+        if (!(await commandIsOwner(interaction, id))) return;
+
+        const { rowCount } = await db.query(
+          "DELETE FROM DOMAINS WHERE ORGANIZATION = $1;",
+          [id]
+        );
+
+        await interaction.reply({
+          content: `Deleted ${rowCount} domains.`,
+          ephemeral: true,
+        });
+      },
+    },
+    {
+      data: new SlashCommandSubcommandBuilder()
+        .setName("list")
+        .setDescription("Lists the domains in the organization")
+        .addStringOption(
+          new SlashCommandStringOption()
+            .setName("id")
+            .setDescription("Short identifier for the organization")
+            .setRequired(true)
+        ),
+      execute: async (interaction) => {
+        await interaction.reply("TODO");
+      },
+    }
+  )
+);
